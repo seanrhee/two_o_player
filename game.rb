4a2044762d2221e23752ee2c79f5f231e9ea29c3
Sean Lee <lee.sean.cw@gmail.com>
2023-03-07 18:23:23 -0500

    add gameover method

--- game.rb
+++ game.rb
@@ -5,5 +5,12 @@ class Game
     @player2 = 3
   end
 
-  
+  def game_over
+    if player1 == 0
+      puts "Player 2 wins with a score of #{player2}/3"
+    elsif player2 = 0
+      puts "Player 1 wins with a score of #{player1}/3"
+    end
+    puts "----- GAME OVER -----"
+  end
 end
